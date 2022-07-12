02498c78a9be12f837593cd49a61d7cde10b55fa
etuong <etuong@gmail.com>
2022-07-12 16:50:10 -0700

    Refactor fork and clean up

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,7 @@
-// const path = require('path');
-
-// module.exports = {
-//   context: __dirname,
-//   entry: './lib/flappy_sheen.js',
-//   output: {
-//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-//     filename: './lib/bundle.js'
-//   },
-//   resolve: {
-//     extensions: ['.js', '.jsx', '*']
-//   },
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.jsx?$/,
-//         exclude: /(node_modules)/,
-//         loader: 'babel-loader',
-//         query: {
-//           presets: ['react', 'es2015']
-//         }
-//       }
-//     ]
-//   },
-//   devtool: 'source-map'
-// };
-
 module.exports = {
-    entry: "./lib/flappy_sheen.js",
-    output: {
-        filename: "./lib/bundle.js"
-    },
-    devtool: 'source-map',
-  };
-  
+  entry: "./lib/flappy_sheen.js",
+  output: {
+    filename: "./lib/bundle.js",
+  },
+  devtool: "source-map",
+};
